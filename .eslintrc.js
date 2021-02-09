da1740402552f3b910772a2fb34dc780bd72313c
Leonardo Faria <leonardofaria@gmail.com>
2021-02-08 22:50:59 -0800

    Add ESLint, Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'leozera',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
