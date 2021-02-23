1dc168984a963bf26ccc6b50ee7b09126ad4bfd1
Leonardo Faria <leonardofaria@gmail.com>
2021-02-22 20:35:50 -0800

    Add Jest and React Testing Library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  extends: [
-    'leozera',
-  ],
+  extends: ['leozera'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -10,14 +8,14 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     /* Turning off for now since React 17 doesn't require import */
     'react/react-in-jsx-scope': 'off',
     /* Disabling it for now til review eslint-config-leozera */
     'react/require-default-props': 'off',
+  },
+  env: {
+    jest: true,
   },
 };
