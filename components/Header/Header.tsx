f948dffe116d410bb1fb94c8015a059a7e850c92
Leonardo Faria <leonardofaria@gmail.com>
2021-02-24 22:32:08 -0800

    Refactor Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,25 +11,27 @@ const Header = () => {
   return (
     <header className="sticky top-0 bg-white dark:bg-black border-b dark:border-gray-600 z-50 bg-opacity-90">
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between py-4 flex-wrap">
+        <div className="relative flex items-center justify-between md:justify-center py-4 flex-wrap">
           <Logo />
 
           <button
             aria-label="Show mobile menu"
-            className="sm:hidden cursor-pointer"
+            className="sm:hidden cursor-pointer inline-flex items-center justify-center w-10 h-10 text-black dark:text-white bg-gray-100 dark:bg-gray-700 rounded-md"
             type="button"
             onClick={() => setIsOpen(!isOpen)}
           >
-            <MdMenu />
+            <MdMenu className="w-5 h-5" />
           </button>
 
           <nav
             aria-label="Main navigation"
             className={`${
-              isOpen ? 'fixed top-16 w-full bg-white dark:bg-black ' : 'hidden'
-            } md:flex flex-grow border-t md:border-t-0 md:py-0 md:mt-0 md:ml-5`}
+              isOpen
+                ? 'fixed top-16 right-0 bg-white dark:bg-black p-5 flex flex-col md:flex-row'
+                : 'hidden'
+            } md:flex flex-grow md:py-0 md:mt-0 md:ml-5`}
           >
-            <ul className="flex flex-col md:flex-row flex-grow md:items-center">
+            <ul className="flex flex-col md:flex-row flex-grow md:items-center md:justify-center">
               <li className="md:mx-2">
                 <Link href="/categories/computer">
                   <span className="flex items-center mr-1 cursor-pointer">
@@ -38,7 +40,7 @@ const Header = () => {
                   </span>
                 </Link>
               </li>
-              <li className="md:mx-2 flex-grow">
+              <li className="md:mx-2">
                 <Link href="/categories/idevice">
                   <span className="flex items-center mr-1 cursor-pointer">
                     <MdTabletMac className="text-lg mr-1" />
@@ -46,10 +48,12 @@ const Header = () => {
                   </span>
                 </Link>
               </li>
-              <li className="mt-2 pt-4 border-t-2 md:mt-0 md:pt-0 md:border-t-0">
+            </ul>
+            <ul className="flex flex-row md:items-center">
+              <li className="mt-2 pt-4 md:mt-0 md:pt-0">
                 <Button href="/about">About</Button>
               </li>
-              <li className="mt-2 pt-4 border-t-2 md:mt-0 md:pt-0 md:border-t-0">
+              <li className="mt-2 pt-4 md:mt-0 md:pt-0">
                 <ThemeChanger />
               </li>
             </ul>
